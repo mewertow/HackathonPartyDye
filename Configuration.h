e2833114286fd1bf30c4e411d3f9fa30842b63f6
Matthew Ewertowski <mattewertowski@gmail.com>
2023-07-22 12:04:53 -0400

    feather board functioning

---
+++ Configuration.h
@@ -0,0 +1,8 @@
+#include <Arduino.h>
+#include <Stepper.h>
+#include <Adafruit_MotorShield.h> 
+#define DEFAULT_TARGET_TEMP 90 //90C 
+#define LIMIT_LOW 10
+#define LIMIT_HIGH 9
+#define LIMIT_PIN 11
+
--- HackathonPartyDye.ino
+++ HackathonPartyDye.ino
@@ -1,12 +1,33 @@
-#include "global.h"
+// #include "src/heating.h"
+// #include "libraries/DallasTemperature-3.9.0/DallasTemperature.h"
+// #include "libraries/OneWire-2.3.7/OneWire.h"
+#include "motion.h"
+#include "Configuration.h"
 
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  Serial.begin(9600);
+    // Serial.println("PARTY TIME");
+  if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
+  // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
+    Serial.println("Could not find Motor Shield. Check wiring.");
+  while (1);
+  }
+  Serial.println("Motor Shield found.");
+  myMotor->setSpeed(10);  // 10 rpm
 
+  pinMode(9, OUTPUT); 
+  pinMode(10, OUTPUT);
+  pinMode(11, INPUT);
+  digitalWrite(9, HIGH);
+  pinMode(10, LOW);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  // home(myMotor);
+  raise(myMotor);
 }
